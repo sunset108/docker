c922ee2b92bae5898cf1ec29ff7323d3041586c3
sunset108 <sunset108@yeah.net>
2014-07-18 16:32:56 +0800

    connect tomcat and mysql

--- tomcat/run-server.sh
+++ tomcat/run-server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo docker run -d -p 8080:8080 -v /data/tomcat:/tomcat/webapps/ROOT -e TOMCAT_PASS="a" sunset108/tomcat
+TAG="mysql"
+CONTAINER_NAME=$(docker ps | grep $TAG | awk '{print $NF}')
+sudo docker run -d -p 8080:8080 -v /data/tomcat:/tomcat/webapps/ROOT -e TOMCAT_PASS="a" --link=${CONTAINER_NAME}:mysql sunset108/tomcat
